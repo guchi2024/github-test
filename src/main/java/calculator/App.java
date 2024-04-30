459899889edc9ce042a58d60bb9afee534ced78c
Robbie <robbie331@>
2024-04-30 10:48:37 +0900

    3번 문제

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -18,7 +18,6 @@ public class App {
         System.out.print("사칙 연산 기호를 입력하세요: ");
         char c = sc.next().charAt(0);
 
-
         // a값 양수
         if (a>=0) {
             System.out.println(a);
@@ -32,6 +31,25 @@ public class App {
             System.out.println("두번째 숫자에 양수를 입력해주세요: ");
         }
 
+        int result = 0;
+
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+      if (c == '+') {
+          result = (a + b);
+      } else if (c == '-'){
+          result = (a - b);
+        }else if (c == '*'){
+          result = (a * b);
+      }else if (c == '/') {
+          if (b == 0){
+              System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+          } else {
+          result = (a / b);}
+      } else {
+          System.out.println(" 사칙 연산자가 아닙니다 ");
+      }
+        System.out.println("결과: " + result);
         }
-        }
+    }
+
 
